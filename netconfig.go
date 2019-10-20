c098849cf40d846edd9b4dc58e8e488acdd50028
Reza <reza@ies-italia.it>
2019-10-20 14:46:46 +0330

    Gateway Mac

--- netconfig.go
+++ netconfig.go
@@ -127,7 +127,7 @@ func (network *Network) getLinux() {
 	out, err = exec.Command("arp", "-e", network.DefaultGateway.String()).Output()
 	if err == nil {
 		lines := strings.Split(string(out), "\n")
-		fmt.Println(lines)
+		fmt.Println(lines[1])
 		if len(lines) == 2 {
 			fmt.Println(strings.Fields(lines[1]))
 			network.DefaultGatewayHardwareAddress, _ = net.ParseMAC(strings.Fields(lines[1])[2])
